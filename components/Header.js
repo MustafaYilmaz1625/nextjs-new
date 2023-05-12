746fd643ace9a9e590ca2a2ade9250ffe72cfd63
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2023-05-13 00:38:41 +0300

    css

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import styles from "../styles/Header.module.css"
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Header.module.css";
+
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
   return (
     <div>
-    <nav>
-      <ul>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/product">Product</Link></li>
-      
-      </ul>
-    </nav>
+      <nav className={styles.title}>
+        <ul>
+          {/* <li><Link href="/">Home</Link></li> */}
+          <li onClick={() => router.push("/")} style={{ cursor: "pointer" }}>
+            Home
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/product">Product</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
